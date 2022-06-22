a8ec236ebeae63ecfdb7006e6e3ecb26c6b81bce
DKatab <101578930+DKatab@users.noreply.github.com>
2022-06-22 13:36:25 -0500

    finalizado

--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,32 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+from statistics import mean
+import sys
+
+if __name__ == '__main__':
+
+    curkey = None
+    value_list = []
+
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = float(val)
+        
+        if key == curkey:
+            value_list.append(val)
+        else:
+            if curkey is not None:
+                suma = sum(value_list)
+                avg = mean(value_list)
+                value_list.clear()                
+
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, suma, avg))
+
+            curkey = key
+            value_list.append(val)               
+
+    suma = sum(value_list)
+    avg = mean(value_list)
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, suma, avg))
